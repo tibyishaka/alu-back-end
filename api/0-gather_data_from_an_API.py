1dfa75e8c9e0242893627a1b642201b45fdf28e7
ibyishaka <t.ibyishaka@alustudent.com>
2025-02-13 08:29:39 +0200

    gather data from an api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,29 +1,35 @@
 #!/usr/bin/python3
-"""Module"""
+"""Script that retrieves an employee's TODO list progress from an API."""
 
 import requests
 import sys
 
-"""Module"""
-
 if __name__ == '__main__':
-    """IF SCRIPT IS NOT RUN AS MODULE"""
+    # Retrieve the employee ID from the command line arguments
     employee_id = sys.argv[1]
-    user_url = "https://jsonplaceholder.typicode.com/users/{}" \
-        .format(employee_id)
-    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
-        .format(employee_id)
 
-    user_info = requests.get(user_url).json()
-    todos_info = requests.get(todos_url).json()
+    # Build the URLs to fetch user information and TODO list data
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)
 
+    # Request data from the API endpoints
+    user_response = requests.get(user_url)
+    todos_response = requests.get(todos_url)
+
+    # Parse the JSON responses
+    user_info = user_response.json()
+    todos_info = todos_response.json()
+
+    # Extract employee name, total tasks and completed tasks
     employee_name = user_info["name"]
-    task_completed = list(filter(lambda obj:
-                                 (obj["completed"] is True), todos_info))
-    number_of_done_tasks = len(task_completed)
-    total_number_of_tasks = len(todos_info)
+    total_tasks = len(todos_info)
+    completed_tasks = [task for task in todos_info if task.get("OK") is True]
+    number_of_completed_tasks = len(completed_tasks)
 
-    print("Employee {} is done with tasks({}/{}):".
-          format(employee_name, number_of_done_tasks, total_number_of_tasks))
+    # Print out the progress summary
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, number_of_completed_tasks, total_tasks))
 
-    [print("\t " + task["title"]) for task in task_completed]
+    # Print the title of each completed task with a tab and a space
+    for task in completed_tasks:
+        print("\t " + task["title"])
