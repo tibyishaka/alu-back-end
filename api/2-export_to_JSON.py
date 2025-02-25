7624b0961ed7c1f693036b34e0040f185e817658
tibyishaka <t.ibyishaka@alustudent.com>
2025-02-25 14:19:24 +0200

    Update 2-export_to_JSON.py

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,56 +1,60 @@
 #!/usr/bin/python3
 """
-    python script that exports data in the JSON format
+Module to fetch user information and export TODO list to a JSON file
 """
 import json
 import requests
-from sys import argv
+import sys
+
+
+def get_employee_info(employee_id):
+    """
+    Get employee information by employee ID
+    """
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    response = requests.get(url)
+    return response.json()
+
+
+def get_employee_todos(employee_id):
+    """
+    Get the TODO list of the employee by employee ID
+    """
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    response = requests.get(url)
+    return response.json()
+
+
+def export_to_json(employee_id, todos):
+    """
+    Export TODO list to a JSON file
+    """
+    filename = f"{employee_id}.json"
+    with open(filename, "w") as file:
+        json.dump({employee_id: todos}, file)
+
+
+def main(employee_id):
+    """
+    Main function to fetch user info and TODO list, then export to JSON
+    """
+    user_info = get_employee_info(employee_id)
+    todos_info = get_employee_todos(employee_id)
+
+    employee_username = user_info["username"]
+
+    todos_info_sorted = [
+        {
+            "task": task["title"],
+            "completed": task["completed"],
+            "username": employee_username
+        } for task in todos_info
+    ]
+
+    export_to_json(employee_id, todos_info_sorted)
 
 if __name__ == "__main__":
-    """
-        request user info by employee ID
-    """
-    request_employee = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}/'.format(argv[1]))
-    """
-        convert json to dictionary
-    """
-    user = json.loads(request_employee.text)
-    """
-        extract username
-    """
-    username = user.get("username")
-
-    """
-        request user's TODO list
-    """
-    request_todos = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1]))
-    """
-        dictionary to store task status(completed) in boolean format
-    """
-    tasks = {}
-    """
-        convert json to list of dictionaries
-    """
-    user_todos = json.loads(request_todos.text)
-    """
-        loop through dictionary & get completed tasks
-    """
-    for dictionary in user_todos:
-        tasks.update({dictionary.get("title"): dictionary.get("completed")})
-
-    task_list = []
-    for k, v in tasks.items():
-        task_list.append({
-            "task": k,
-            "completed": v,
-            "username": username
-        })
-
-    json_to_dump = {argv[1]: task_list}
-    """
-        export to JSON
-    """
-    with open('{}.json'.format(argv[1]), mode='w') as file:
-        json.dump(json_to_dump, file)
+    if len(sys.argv) > 1:
+        main(sys.argv[1])
+    else:
+        print("Usage: ./2-export_to_JSON.py <employee_id>")
